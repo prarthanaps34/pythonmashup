79e1a5e30c72988dae346567b1410ab99bb987c2
Prarthana P.S <prarthanaps141@gmail.com>
2026-09-10 12:49:02 +0530

    Add cw4.py with initial inventory management logic for fruits, vegetables, and beverages

---
+++ cw4.py
@@ -0,0 +1,23 @@
+fruits=['apple','orange','grapes'];
+vegetables=['tomato','chilli','potato']
+beverages=['water','coke','orangejuice'];
+fruits.append('banana');
+print(fruits);
+vegetables.insert(1,'cucumber');
+print(vegetables);
+del beverages[2]
+print(beverages);
+inventory=[fruits,vegetables,beverages]
+print(inventory);
+print(fruits[:2]);
+print(vegetables[-1]);
+length=[len(x) for x in fruits];
+print(length);
+if 'water' in beverages:
+    print("yes,'water' is in list");
+products=(fruits[0],vegetables[0],beverages[0])   ;
+print(products)
+
+
+
+
